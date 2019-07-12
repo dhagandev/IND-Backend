f039e947d5598a4eadbb59e6b363816991355f5e
dhagandev <dhagan@utexas.edu>
2019-07-11 21:15:17 -0500

    Can read from db not sure if I can post

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const admin = require('firebase-admin');
+
+let serviceAccount = require('../config/narrative-design.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount)
 });
+
+let db = admin.firestore();
+
+/* GET users listing. */
+router.get('/test', function(req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/test/getAllUsers', function(req, res, next) {
+	let usersRef = db.collection('UserProfile')
+	let usersResult = {}
+	let allUsers = usersRef.get()
+		.then(snapshot => {
+			snapshot.forEach(doc => {
+				usersResult[doc.id] = doc.data()
+			})
+		})
+		.then(() => {
+			res.send({data: usersResult});
+		})
+})
+
+router.post('/test/postNewUser', function(req, res, next) {
+	let usersRef = db.collection('UserProfile')
+	let postExample = {
+		email: "2-testemail@gmail.com",
+		emailVerified: false,
+		name: "2-profile-test-name",
+		role: "writer"
+	}
+	console.log(req.body)
+	let reqPost = {}
+	//Add will autogen id
+	//Data will come from req.body
+	// usersRef.add(postExample);
+})
 
 module.exports = router;
